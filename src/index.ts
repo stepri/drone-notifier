7a212ee10992466848ab8f4a1739dad5d5328bab
Steven Prins <steven@kaartje2go.nl>
2020-09-10 21:33:30 +0200

    fix linting

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {
   doRequest, sendNotification
 } from './util';
 
-const intervalInSeconds = parseInt(process.env.DRONE_INTERVAL) || 8; 
+const intervalInSeconds = parseInt(process.env.DRONE_INTERVAL, 2) || 8;
 
 const cachedBuilds = {} as { [key: string]: Build[]; };
 
@@ -10,13 +10,16 @@ async function handler() {
   try {
     const user = await doRequest('/api/user') as User;
 
-    const repos = await doRequest('/api/user/repos?latest=true') as Array<Repo>;
+    const repos = await doRequest('/api/user/repos?latest=true') as Repo[];
     const activeRepos = repos.filter(repo => repo.active);
 
     for (const repoIndex in activeRepos) {
+      if (!activeRepos.hasOwnProperty(repoIndex)) {
+        continue;
+      }
       const repo = repos[repoIndex];
 
-      const builds = await doRequest(`/api/repos/${repo.slug}/builds?page=1`) as Array<Build>;
+      const builds = await doRequest(`/api/repos/${repo.slug}/builds?page=1`) as Build[];
       const buildsFromUser = builds.filter(build => build.author_login === user.login);
       if (buildsFromUser.length === 0) {
         continue;
@@ -29,13 +32,16 @@ async function handler() {
       }
 
       for (const buildId in buildsFromUser) {
+        if (!buildsFromUser.hasOwnProperty(buildId)) {
+          continue;
+        }
         const build = buildsFromUser[buildId];
 
         const cachedBuild = cachedBuilds[repo.slug].find(b => b.id === build.id);
         if (!cachedBuild) {
           sendNotification(
-            `${repo.slug} - ${build.source}`, 
-            `Started new build!`, 
+            `${repo.slug} - ${build.source}`,
+            `Started new build!`,
             `/${repo.slug}/${build.number}`)
           continue;
         }
@@ -45,14 +51,14 @@ async function handler() {
         }
 
         sendNotification(
-          `${repo.slug} - ${build.source}`, 
-          `${cachedBuild.status} -> ${build.status}`, 
+          `${repo.slug} - ${build.source}`,
+          `${cachedBuild.status} -> ${build.status}`,
           `/${repo.slug}/${build.number}`)
       }
 
       cachedBuilds[repo.slug] = buildsFromUser;
     }
-  
+
     console.log(`Retry in ${intervalInSeconds} seconds`);
     setTimeout(handler, intervalInSeconds * 1000);
   } catch (error) {
@@ -66,11 +72,11 @@ async function handler() {
     console.error('please provider a drone host');
     return;
   }
-  
+
   if (!process.env.DRONE_API_KEY) {
     console.error('please provider a api key');
     return;
   }
-  
+
   await handler()
 })();
