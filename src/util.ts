962d7e5e1f236a4d1f0032b0fb3b7573006facb8
Steven Prins <steven@kaartje2go.nl>
2020-09-10 21:28:34 +0200

    fix tabs

--- src/util.ts
+++ src/util.ts
@@ -4,8 +4,8 @@ import notifier from 'node-notifier';
 export async function doRequest(url: string) {
   url = `${process.env.DRONE_HOST}${url}`;
 
-	try {
-		const response = await got(url, {
+  try {
+    const response = await got(url, {
       responseType: 'json',
       headers: {
         Authorization: `Bearer ${process.env.DRONE_API_KEY}`
@@ -14,10 +14,10 @@ export async function doRequest(url: string) {
     if (response.statusCode !== 200) {
       throw new Error(`Could not fetch data ${url}, response: ${response.body}`)
     }
-		return response.body;
-	} catch (error) {
+    return response.body;
+  } catch (error) {
     throw new Error(`Could not fetch data ${url}, error: ${error}`)
-	}
+  }
 }
 
 export function sendNotification(title: string, message: string, url?: string) {
